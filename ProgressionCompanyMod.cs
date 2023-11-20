ceaf5913347c7da7d740eb0233d3cbe473dca52f
Nathan <37817019+NathanPeake@users.noreply.github.com>
2023-11-20 17:04:13 +0000

    First test to make sure loading and depends all work

--- ProgressionCompanyMod.cs
+++ ProgressionCompanyMod.cs
@@ -1,5 +1,9 @@
 ﻿using BepInEx;
+using HarmonyLib;
 using System;
+using static UnityEngine.UIElements.GenericDropdownMenu;
+using UnityEngine.UIElements;
+using ShadowUtilityLIB.logging;
 
 namespace LCPC
 {
@@ -12,6 +16,34 @@ namespace LCPC
     [BepInPlugin(ModInfo.ModID, ModInfo.ModName, ModInfo.ModVersion)]
     public class ProgressionCompanyMod : BaseUnityPlugin
     {
-
+        void Awake()
+        {
+            Awake();
+        }
+    }
+    public static class ProgressionCompany
+    {
+        private static Logger logger = new Logger(ModInfo.ModID, ModInfo.ModVersion);
+        public static void Awake()
+        {
+            logger.Log("Starting");
+            Harmony.CreateAndPatchAll(typeof(ProgressionCompanyPatchers));
+            logger.Log("Patched");
+            logger.Log("finished init");
+        }
+        public static void SetupGame()
+        {
+            logger.Log("Starting game setup");
+        }
+    }
+    public static class ProgressionCompanyPatchers
+    {
+        [HarmonyPatch(typeof(MenuManager))]
+        [HarmonyPatch("ConfirmHostButton")]
+        [HarmonyPostfix]
+        public static void MenuManager_ConfirmHostButton()
+        {
+            ProgressionCompany.SetupGame();
+        }
     }
 }
