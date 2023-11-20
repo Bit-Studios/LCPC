8733f039d5ae478b2eaf378ae0aaffce23c4f3f7
Nathan <37817019+NathanPeake@users.noreply.github.com>
2023-11-20 14:02:18 +0000

    Add project files.

---
+++ ProgressionCompanyMod.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using System;
+
+namespace LCPC
+{
+    sealed public class ModInfo
+    {
+        public const string ModID = "com.shadowdev.lcpc";
+        public const string ModName = "progressioncompany";
+        public const string ModVersion = "0.0.1";
+    }
+    [BepInPlugin(ModInfo.ModID, ModInfo.ModName, ModInfo.ModVersion)]
+    public class ProgressionCompanyMod : BaseUnityPlugin
+    {
+
+    }
+}
